c6026276dc274b785cc4cdebd65b74b83ba5ffb4
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-07-02 20:01:03 -0400

    Update Sighting.js

--- src/pages/Sighting.js
+++ src/pages/Sighting.js
@@ -7,7 +7,11 @@ const Sighting = () => {
                 <label htmlFor="sharkType">Shark Type: </label><br></br>
                 <input type="text" id="sharkType" name="sharkType"></input><br></br>
                 <label htmlFor="sightingLocation">Location: </label><br></br>
-                <input type="text" id="sightingLocation" name="sightingLocation"></input>
+                <input type="text" id="sightingLocation" name="sightingLocation"></input><br></br>
+                <label htmlFor="email">Email: </label><br></br>
+                <input type="text" id="email" name="email"></input><br></br>
+                <label htmlFor="signUpForEmail">Join SharkStewards Newsletter</label><br></br>
+                <input type="checkbox" id="sightingLocation" name="sightingLocation"></input>
             </form>
         </div>
     )
