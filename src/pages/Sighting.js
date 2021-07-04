e7a998737cdc70b860a2d564adf7ed141505f5ea
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-07-04 19:00:18 -0400

    changed input values

--- src/pages/Sighting.js
+++ src/pages/Sighting.js
@@ -3,15 +3,21 @@ import React from "react";
 const Sighting = () => {
     return(
         <div id="sightingContainer">
-            <form id="sightingForm">
+            <form name="sightingForm" id="sightingForm">
                 <label htmlFor="sharkType">Shark Type: </label><br></br>
-                <input type="text" id="sharkType" name="sharkType"></input><br></br>
+                <select name="sharkTypes" id="sharkTypes" form="sightingForm">
+                    <option value="SevenGill Shark">SevenGill Shark</option>
+                    <option value="School Shark">School Shark</option>
+                    <option value="Horn Shark">Horn Shark</option>
+                    <option value="Pacific Angel Shark">Pacific Angel Shark</option>
+                    <option value="Great White Shark">Great White Shark</option>
+                    <option value="Leopard Shark">Leopard Shark</option>
+                    <option value="Short Finned Mako Shark">Short Finned Mako Shark</option>
+                </select><br></br>
                 <label htmlFor="sightingLocation">Location: </label><br></br>
-                <input type="text" id="sightingLocation" name="sightingLocation"></input><br></br>
-                <label htmlFor="email">Email (Optional): </label><br></br>
-                <input type="text" id="email" name="email"></input><br></br>
-                <label htmlFor="signUpForEmail">Join SS Newsletter</label>
-                <input type="checkbox" id="sightingLocation" name="sightingLocation"></input><br></br>
+                <input className="sightingInput" type="text" id="sightingLocation" name="sightingLocation"></input><br></br>
+                <label htmlFor="date">Time: </label><br></br>
+                <input className="sightingInput" type="date" id="date" name="date"></input><br></br>
                 <input className="submitSightingReport" type="submit" value="Submit" />
             
 
