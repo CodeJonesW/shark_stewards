8e1628c1cdd0e52992f6190ead702ce067d5a8f7
Tanishq Kancharla <tklightningfast@gmail.com>
2021-07-07 23:21:27 -0400

    Worked on creating a shark picker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Form";
-import Sighting from "./components/Sighting_Form.js";
+import Form from "./pages/Form";
+// import Sighting from "./components/Sighting_Form.js";
 import Navbar from "./components/Navbar.js";
 import Footer from "./components/Footer.js";
 import { Flex, View } from "@adobe/react-spectrum";
@@ -12,8 +12,8 @@ function App() {
             <Navbar />
             <Router>
                 <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/sighting" component={Sighting} />
+                    <Route exact path="/" component={View} />
+                    <Route exact path="/sighting" component={Form} />
                     <Route
                         render={() => (
                             <h1 className="display-2">
