8156592fe9ee92c7b8f74c4669014afd59091c40
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-07-14 23:09:32 -0400

    commented out localstorage functionality for now

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -9,32 +9,34 @@ const LS = {
     },
 
     checkSightingData: async (data) => {
-        console.log(moment().format('hh:mm:ss a'))
-        // if the sightings exist in local storage
-        if(localStorage.getItem('sightings')){
-            // grab the time the data was last stored
-            const previousTime = localStorage.getItem('timeOfLastSightingsLoad')
-            // if previous and current day are the same and if the current hour 
-            // is still the same... grab stored sightings
-            if(parseInt(previousTime) === parseInt(moment().format('hh')) ){ 
-                console.log("GRABBED FROM LS")         
-                return JSON.parse(localStorage.getItem('sightings')) 
-            } else {
-                console.log("GRABBED FROM DB") 
-                // otherwise get the sightings from the server/DB
-                const newSightings = await API.getSightingData()
-                LS.saveSightingData(newSightings)
-                return newSightings
-            }
-        } else {
-            // otherwise get the sightings from the server/DB
-            console.log("GRABBED FROM DB") 
-            const newSightings = await API.getSightingData()
-            LS.saveSightingData(newSightings)
-            return newSightings
-        }
+        // console.log(moment().format('hh:mm:ss a'))
+        // // if the sightings exist in local storage
+        // if(localStorage.getItem('sightings')){
+        //     // grab the time the data was last stored
+        //     const previousTime = localStorage.getItem('timeOfLastSightingsLoad')
+        //     // if previous and current day are the same and if the current hour 
+        //     // is still the same... grab stored sightings
+        //     if(parseInt(previousTime) === parseInt(moment().format('hh')) ){ 
+        //         console.log("GRABBED FROM LS")         
+        //         return JSON.parse(localStorage.getItem('sightings')) 
+        //     } else {
+        //         console.log("GRABBED FROM DB") 
+        //         // otherwise get the sightings from the server/DB
+        //         const newSightings = await API.getSightingData()
+        //         LS.saveSightingData(newSightings)
+        //         return newSightings
+        //     }
+        // } else {
+        //     // otherwise get the sightings from the server/DB
+        //     console.log("GRABBED FROM DB") 
+        //     const newSightings = await API.getSightingData()
+        //     LS.saveSightingData(newSightings)
+        //     return newSightings
+        // }
         
-
+        const newSightings = await API.getSightingData()
+        LS.saveSightingData(newSightings)
+        return newSightings
     }
 }
 
